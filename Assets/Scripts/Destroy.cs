09589f6d97f73057c1371c8e45fc7fc776aac995
huynhcam1 <cameron_huynh@hotmail.com>
2019-10-21 23:39:34 -0700

    Initial Commit

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Destroys all previously spawned platforms.
+ */
+public class Destroy : MonoBehaviour
+{
+    //public GameObject player;
+    public GameObject platformPrefab;
+    private GameObject newPlatform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        float x = collider.transform.position.x;
+        float y = collider.transform.position.y;
+        Vector2 position = new Vector2(x, y - Random.Range(1f, 2f));
+        newPlatform = Instantiate(platformPrefab, position, Quaternion.identity);
+        Destroy(collider.gameObject);
+        Debug.Log("break");
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Script to allow movement by touching the screen.
+ */
+ [RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    // movement scale
+    public float movementSpeed = 1f;
+
+    // enables physics
+    Rigidbody2D rb;
+
+    float movement = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+        //if (Input.touchCount > 0)
+        //{
+        //    Touch touch = Input.GetTouch(0);
+        //    Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+        //    touchPosition.z = 0f;
+        //    transform.position = touchPosition;
+        //}
+    }
+
+    // movement
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
